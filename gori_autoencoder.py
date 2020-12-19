857ccb56ca6f7b999ffb2ff4d2625329749995aa
konishi0125 <yumemita@gmail.com>
2020-12-19 18:20:32 +0900

    モデルの保存追加

--- gori_autoencoder.py
+++ gori_autoencoder.py
@@ -13,15 +13,19 @@ from keras.engine.topology import Input
 from PIL import Image
 
 IMG_SIZE=64
+GRAY_SCALE=True
 
 # CNN設定
 BATCH_SIZE = 64
-EPOCHS = 200
+EPOCHS = 1
 LEARNING_RATE = 0.0001
 
 def model_train(train, test):
     # モデルの定義
-    input_img = Input(shape=(IMG_SIZE,IMG_SIZE,1))
+    if GRAY_SCALE is True:
+        input_img = Input(shape=(IMG_SIZE,IMG_SIZE,1))
+    else:
+        input_img = Input(shape=(IMG_SIZE,IMG_SIZE,3))
     x = Conv2D(64,(3,3),padding="same")(input_img)
     x = BatchNormalization()(x)
     x = Activation("relu")(x)
@@ -33,7 +37,7 @@ def model_train(train, test):
     x = Conv2D(16,(3,3),padding="same")(x)
     x = BatchNormalization()(x)
     x = Activation("relu")(x)
-    encoded = MaxPooling2D((2,2), padding="same")(x)
+    encoded = MaxPooling2D((2,2), padding="same", name="encoder_layer")(x)
 
     x = Conv2D(16,(3,3),padding="same")(encoded)
     x = BatchNormalization()(x)
@@ -47,27 +51,42 @@ def model_train(train, test):
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = UpSampling2D((2, 2))(x)
-    x = Conv2D(1, (3, 3), padding='same')(x)
+    if GRAY_SCALE is True:
+        x = Conv2D(1, (3, 3), padding='same')(x)
+    else:
+        x = Conv2D(3, (3, 3), padding='same')(x)
     x = BatchNormalization()(x)
     decoded = Activation("sigmoid")(x)
     
-    encoder = Model(input_img, encoded)
+    
     model = Model(input_img, decoded)
     model.compile(optimizer="adam", loss='binary_crossentropy')
 
-
-    history = model.fit(train, train,\
-        batch_size=BATCH_SIZE, epochs=EPOCHS,\
+    #学習の実行
+    model.fit(train, train, batch_size=BATCH_SIZE, epochs=EPOCHS,\
              validation_data=(test, test), shuffle=True)
+    encoder = Model(inputs=input_img, outputs=model.get_layer("encoder_layer").output)
+
+    #モデルの保存
+    encoder.save("./encoder.h5")
+    model.save("./autoencoder.h5")
+
+    #確認用の描画　なくても良い
+    if GRAY_SCALE is True:
+        gen_test = model.predict(test[0].reshape(1,IMG_SIZE,IMG_SIZE,1))
+        plt.gray()
+        plt.imshow(test[0].reshape(IMG_SIZE,IMG_SIZE))
+        plt.show()
+        plt.gray()
+        plt.imshow(gen_test[0].reshape(IMG_SIZE,IMG_SIZE))
+        plt.show()
     
-    #print(encoder.predict(test[0].reshape(1,32,32,3)))
-    gen_test = model.predict(test[0].reshape(1,IMG_SIZE,IMG_SIZE,1))
-    plt.gray()
-    plt.imshow(test[0].reshape(IMG_SIZE,IMG_SIZE))
-    plt.show()
-    plt.gray()
-    plt.imshow(gen_test[0].reshape(IMG_SIZE,IMG_SIZE))
-    plt.show()
+    else:
+        gen_test = model.predict(test[0].reshape(1,IMG_SIZE,IMG_SIZE,3))
+        plt.imshow(test[0].reshape(IMG_SIZE,IMG_SIZE,3))
+        plt.show()
+        plt.imshow(gen_test[0].reshape(IMG_SIZE,IMG_SIZE,3))
+        plt.show()
 
     
 
